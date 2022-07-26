418c0da6d01b6a46116f77009b68e7a3ed1f3232
hayeonkimm <hayeon.data@gmail.com>
2022-07-26 11:10:12 +0900

    업데이트 -  travis.yml 추가

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const connect = require('./src/model/db');
 const morgan = require('morgan');
-//보안
+//보안.
 const cors = require('cors');
 const helmet = require('helmet');
 //라우터
